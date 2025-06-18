0a2e608531f07347c536b8780153ded0c2210499
stiigp <lucaspanuccidev@gmail.com>
2025-06-18 16:23:02 -0300

    Feat: Implementando a classe AppCleaner para evitar triplicação de todas as fotos em que a data é adicionada.

--- src/main/java/org/example/piclabel/MainApplication.java
+++ src/main/java/org/example/piclabel/MainApplication.java
@@ -4,13 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.piclabel.utils.AppCleaner;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
+        Path processedPath = Paths.get(System.getProperty("user.home"), "PicLabel", "processed");
+        AppCleaner.setupShutdownHook(processedPath);
         Scene scene = new Scene(fxmlLoader.load(), 960, 720);
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/org/example/piclabel/utils/AppCleaner.java
@@ -0,0 +1,36 @@
+package org.example.piclabel.utils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.Comparator;
+
+
+public class AppCleaner {
+
+    public static void setupShutdownHook(Path folderPath) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                deleteFolderContents(folderPath);
+                System.out.println("Pasta temporária limpa com sucesso.");
+            } catch (IOException e) {
+                System.err.println("Erro ao limpar pasta temporária: " + e.getMessage());
+            }
+        }));
+    }
+
+    private static void deleteFolderContents(Path folderPath) throws IOException {
+        if (!Files.exists(folderPath)) return;
+
+        Files.walk(folderPath)
+                .filter(p -> !p.equals(folderPath))
+                .sorted(Comparator.reverseOrder()) // remove arquivos antes de diretórios
+                .forEach(path -> {
+                    try {
+                        Files.delete(path);
+                    } catch (IOException e) {
+                        System.err.println("Erro ao excluir " + path + ": " + e.getMessage());
+                    }
+                });
+    }
+}
